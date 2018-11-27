abc37778d06f9715017e70abf5f94fd49163f5cb
Ekaterina Ryzhova <rubicante@icloud.com>
2018-11-27 21:11:09 +0300

    Initial Commit

---
+++ GPGPU/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GPGPU
+//
+//  Created by kathelyss on 27/11/2018.
+//  Copyright © 2018 Екатерина Рыжова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
